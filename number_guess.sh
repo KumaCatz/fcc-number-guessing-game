9a39b6bd51f48a3968e5a7f426e9c86b642152c4
KumaCatz K <tod.kagan@gmail.com>
2024-06-21 16:27:18 +0000

    feat: database-created

--- number_guess.sh
+++ number_guess.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
-RANDOM_NUMBER=$RANDOM
+PSQL="psql --username=freecodecamp --dbname=<database_name> -t --no-align -c"
 
-echo $RANDOM_NUMBER
+RANDOM_NUMBER=$(shuf -i 1-1000 -n 1)
 
+MAIN_MENU() {
+  echo -e "\n~~~~ Number Guessing Game ~~~~\n"
+
+  echo Enter your username:
+  read USERNAME
+  
+}
+
+MAIN_MENU
