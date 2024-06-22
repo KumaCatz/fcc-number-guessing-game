24cf7f6e816718b3ef357f2d82e79725e6b4896e
KumaCatz K <tod.kagan@gmail.com>
2024-06-22 15:21:17 +0000

    feat: clear-tables-file, diff-new-username

--- number_guess.sh
+++ number_guess.sh
@@ -1,14 +1,37 @@
 #!/bin/bash
 
-PSQL="psql --username=freecodecamp --dbname=<database_name> -t --no-align -c"
+PSQL="psql --username=freecodecamp --dbname=number_guess -t --no-align -c"
 
 RANDOM_NUMBER=$(shuf -i 1-1000 -n 1)
+
+PSQL_FULL_JOIN_ALL_TABLES="
+  SELECT
+    *
+  FROM
+    users
+  FULL JOIN
+    games ON users.user_id = games.user_id
+"
 
 MAIN_MENU() {
   echo -e "\n~~~~ Number Guessing Game ~~~~\n"
 
   echo Enter your username:
   read USERNAME
+
+  USERNAME_RESULT=$($PSQL "$PSQL_FULL_JOIN_ALL_TABLES WHERE username='$USERNAME'")
+
+  if [[ -z $USERNAME_RESULT ]]
+  then
+    INSERT_USER_RESULT=$($PSQL "INSERT INTO users (username) VALUES ('$USERNAME')")
+    echo -e "\nWelcome, $USERNAME! It looks like this is your first time here."
+  else
+    echo $USERNAME_RESULT
+    echo $USERNAME_RESULT | while IFS='|' read USER_ID
+    do
+      echo Welcome back, <username>! You have played <games_played> games, and your best game took <best_game> guesses.
+    done
+  fi
   
 }
 
