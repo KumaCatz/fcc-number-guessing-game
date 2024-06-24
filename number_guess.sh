a0cd02c2add13b4a063a67cd1550b034bf3215e8
KumaCatz K <tod.kagan@gmail.com>
2024-06-24 22:23:04 +0000

    feat: number-game-implemented

--- number_guess.sh
+++ number_guess.sh
@@ -2,15 +2,19 @@
 
 PSQL="psql --username=freecodecamp --dbname=number_guess -t --no-align -c"
 
-RANDOM_NUMBER=$(shuf -i 1-1000 -n 1)
+SECRET_NUMBER=$(shuf -i 1-1000 -n 1)
 
 PSQL_FULL_JOIN_ALL_TABLES="
   SELECT
-    *
+    u.user_id AS users_user_id,
+    u.username,
+    u.games_played,
+    g.game_id,
+    g.number_of_guesses
   FROM
-    users
+    users u
   FULL JOIN
-    games ON users.user_id = games.user_id
+    games g ON u.user_id = g.user_id
 "
 
 MAIN_MENU() {
@@ -27,11 +31,35 @@ MAIN_MENU() {
     echo -e "\nWelcome, $USERNAME! It looks like this is your first time here."
   else
     echo $USERNAME_RESULT
-    echo $USERNAME_RESULT | while IFS='|' read USER_ID
+    echo "$USERNAME_RESULT" | while IFS='|' read USER_ID USERNAME GAMES_PLAYED GAME_ID NUMBER_OF_GUESSES
     do
-      echo Welcome back, <username>! You have played <games_played> games, and your best game took <best_game> guesses.
+      echo -e "\nWelcome back, $USERNAME! You have played $GAMES_PLAYED games, and your best game took 0 guesses."
     done
   fi
+
+  echo -e "\nGuess the secret number between 1 and 1000:"
+  read USER_GUESS
+  NUMBER_OF_GUESSES=1
+
+  GUESSING_LOOP() {
+    if [[ $1 -lt $SECRET_NUMBER ]]
+      then
+        echo "It's higher than that, guess again:"
+        read USER_GUESS
+        ((NUMBER_OF_GUESSES++))
+        GUESSING_LOOP "$USER_GUESS"
+    elif [[ $1 -gt $SECRET_NUMBER ]]
+      then
+        echo "It's lower than that, guess again:"
+        read USER_GUESS
+        ((NUMBER_OF_GUESSES++))
+        GUESSING_LOOP "$USER_GUESS"
+    else
+      echo You guessed it in $NUMBER_OF_GUESSES tries. The secret number was $SECRET_NUMBER. Nice job!
+    fi
+  }
+
+  GUESSING_LOOP "$USER_GUESS"
   
 }
 
